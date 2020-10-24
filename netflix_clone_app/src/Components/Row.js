0cad00f713d5b81352dced989fe7ec8585520de1
Petch <petchkubb@live.com>
2020-10-24 13:59:22 +0700

    add youtube trailer

--- netflix_clone_app/src/Components/Row.js
+++ netflix_clone_app/src/Components/Row.js
@@ -3,11 +3,14 @@
 import React, { useState, useEffect } from "react"
 import axios from "../axios"
 import "./Row.css"
+import Youtube from "react-youtube"
+import movieTrailer from "movie-trailer"
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState("")
 
   useEffect(() => {
     async function fetchData() {
@@ -18,7 +21,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData()
   }, [fetchUrl])
 
-  console.table(movies)
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("")
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get("v"))
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -33,9 +58,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 : `${IMG_API}${movies.backdrop_path}`
             }
             alt={movies.name}
+            onClick={() => handleClick(movies)}
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
