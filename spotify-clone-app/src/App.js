608533ac531270b72127dadde31970d7a4418add
Petch <petchkubb@live.com>
2020-10-25 14:06:55 +0700

    add context API

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,21 +1,38 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import "./App.css"
 import Login from "./components/Login"
+import Player from "./components/Player"
 import { getTokenFromUrl } from "./utils/spotify"
+import SpotifyWebApi from "spotify-web-api-js"
+import { useDataLayerValue } from "./context/DataLayer"
 
+const spotify = new SpotifyWebApi()
 function App() {
-  const [token, setToken] = useState(null)
+  const [{ user, token }, dispatch] = useDataLayerValue()
 
   useEffect(() => {
     const hash = getTokenFromUrl()
     window.location.hash = ""
     const _token = hash.access_token
     if (_token) {
-      setToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      spotify.setAccessToken(_token)
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        })
+      })
     }
   }, [])
-
-  return <div className="app">{token ? <h1>I'am Loged in</h1> : <Login />}</div>
+  return (
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  )
 }
 
 export default App
---
+++ spotify-clone-app/src/reducer/reducer.js
@@ -0,0 +1,27 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  token:
+    "BQAgKN5DkFco2zgQ5QDCYyWiDz5duP7AnFtHjw-ggDuEnyTwWC3MEFNv79OSlSKPI6sc0fFRYy_oJuAoYbuZm5CADxwzRWxeh7VvSFIP9x0Txwav-q8_wOCLUZofoMTroo6YgzPuoYa6qosxjuPOx2ViHd-JCUmB7d2Y9-OWj1kH9PQ-",
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      }
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
