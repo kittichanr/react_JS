39faab92e9db977bddb5595b55a930f734d893a2
petchkubbb <>
2020-11-15 10:40:39 +0700

    add ui sidebar

--- slack-clone-app/src/components/Sidebar/index.js
+++ slack-clone-app/src/components/Sidebar/index.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react"
+import "./Sidebar.css"
+import SidebarOption from '../SidebarOption'
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
+import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <h1>Sidebar</h1>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__hearder">
+        <div className="sidebar__info">
+          <h2>React JS Slack</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Kittichan
+          </h3>
         </div>
-    )
+        <CreateIcon/>
+      </div>
+        <SidebarOption Icon={InsertCommentIcon} title="Treads"/>
+        <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+        <SidebarOption Icon={DraftsIcon} title="Save items"/>
+        <SidebarOption Icon={BookmarkBorderIcon} title="Chanel browser"/>
+        <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+        <SidebarOption Icon={AppsIcon} title="Apps"/>
+        <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+        <SidebarOption Icon={ExpandLessIcon} title="Show less"/>       
+        <hr/> 
+        <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+        <hr/> 
+        <SidebarOption Icon={AddIcon} title="Add Channel"/>
+    </div>
+  )
 }
 
 export default Sidebar
