05a52b46958f59510fd555160dda2789869d2e3a
petchkubbb <>
2020-11-29 08:50:33 +0700

    connect firebase

---
+++ slack-clone-app/src/api/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGKjl87xfKn9XkaMK-dn1sJEJUtXPY-kc",
+  authDomain: "slack-clone-app-603fc.firebaseapp.com",
+  databaseURL: "https://slack-clone-app-603fc.firebaseio.com",
+  projectId: "slack-clone-app-603fc",
+  storageBucket: "slack-clone-app-603fc.appspot.com",
+  messagingSenderId: "514302983530",
+  appId: "1:514302983530:web:ec9a39d2b821e6a7a77913",
+  measurementId: "G-T970TK0RR5",
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GithubAuthProvider()
+
+export { auth, provider }
+export default db
--- slack-clone-app/src/components/Sidebar/index.js
+++ slack-clone-app/src/components/Sidebar/index.js
@@ -1,20 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./Sidebar.css"
-import SidebarOption from '../SidebarOption'
+import SidebarOption from "../SidebarOption"
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
-import CreateIcon from '@material-ui/icons/Create';
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import AppsIcon from '@material-ui/icons/Apps';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import AddIcon from '@material-ui/icons/Add';
+import CreateIcon from "@material-ui/icons/Create"
+import InsertCommentIcon from "@material-ui/icons/InsertComment"
+import InboxIcon from "@material-ui/icons/Inbox"
+import DraftsIcon from "@material-ui/icons/Drafts"
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder"
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt"
+import AppsIcon from "@material-ui/icons/Apps"
+import FileCopyIcon from "@material-ui/icons/FileCopy"
+import ExpandLessIcon from "@material-ui/icons/ExpandLess"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import AddIcon from "@material-ui/icons/Add"
+
+import db from "../../api/firebase"
 
 const Sidebar = () => {
+  const [channels, setChannels] = useState([])
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) => {
+      console.log(snapshot.docs)
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    })
+  }, [])
+
   return (
     <div className="sidebar">
       <div className="sidebar__hearder">
@@ -25,20 +41,23 @@ const Sidebar = () => {
             Kittichan
           </h3>
         </div>
-        <CreateIcon/>
+        <CreateIcon />
       </div>
-        <SidebarOption Icon={InsertCommentIcon} title="Treads"/>
-        <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
-        <SidebarOption Icon={DraftsIcon} title="Save items"/>
-        <SidebarOption Icon={BookmarkBorderIcon} title="Chanel browser"/>
-        <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
-        <SidebarOption Icon={AppsIcon} title="Apps"/>
-        <SidebarOption Icon={FileCopyIcon} title="File browser"/>
-        <SidebarOption Icon={ExpandLessIcon} title="Show less"/>       
-        <hr/> 
-        <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
-        <hr/> 
-        <SidebarOption Icon={AddIcon} title="Add Channel"/>
+      <SidebarOption Icon={InsertCommentIcon} title="Treads" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Save items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Chanel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Add Channel" />
+      {channels.map((channel) => (
+        <SidebarOption title={channel.name} id={channel.id}/>
+      ))}
     </div>
   )
 }
