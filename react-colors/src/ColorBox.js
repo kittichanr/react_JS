5876946e6ecdcdcc77156602b12aadda56d39d83
petchkubbb <>
2021-02-14 08:45:58 +0700

    add copy overlay animation

--- react-colors/src/ColorBox.js
+++ react-colors/src/ColorBox.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ColorBox.css"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 
 const ColorBox = ({ background, name }) => {
+  const [copied, setCopied] = useState(false)
+  const onCopy = () => {
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }
   return (
-    <CopyToClipboard text={background}>
+    <CopyToClipboard text={background} onCopy={onCopy}>
       <div style={{ background }} className="ColorBox">
+        <div
+          style={{ background }}
+          className={`copy-overlay ${copied && "show"}`}
+        />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>Copied!</h1>
+          <p>{background}</p>
+        </div>
         <div className="copy-container">
           <div className="box-content">
             <span>{name}</span>
