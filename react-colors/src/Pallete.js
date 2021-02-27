d5bdf1dc6fda4d5233666fb8d69783cedb597a30
petchkubbb <>
2021-02-27 13:45:50 +0700

    add palette footer

--- react-colors/src/Pallete.js
+++ react-colors/src/Pallete.js
@@ -3,12 +3,12 @@ import ColorBox from "./ColorBox"
 import "./Pallete.css"
 import Navbar from "./Navbar"
 
-const Pallete = ({ palette }) => {
+const Pallete = ({ palette, paletteName, emoji }) => {
   const [level, setLevel] = useState(500)
-  const [format, setFormat] = useState('hex')
+  const [format, setFormat] = useState("hex")
 
   const colorBoxes = palette.colors[level].map((color) => (
-    <ColorBox background={color[format]} name={color.name} />
+    <ColorBox background={color[format]} name={color.name} key={color.id}/>
   ))
 
   const changeLevel = (level) => setLevel(level)
@@ -19,8 +19,16 @@ const Pallete = ({ palette }) => {
 
   return (
     <div className="Pallete">
-      <Navbar level={level} changeLevel={changeLevel} handleChange={changeFormat}/>
+      <Navbar
+        level={level}
+        changeLevel={changeLevel}
+        handleChange={changeFormat}
+      />
       <div className="Pallete-colors">{colorBoxes}</div>
+      <footer className="Pallete-footer">
+        {palette.paletteName}
+        <span className="Pallete-emoji">{palette.emoji}</span>
+      </footer>
     </div>
   )
 }
