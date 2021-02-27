9dc7f82ee2335516e7c7ababffe5620fbc758f5b
petchkubbb <>
2021-02-27 15:44:42 +0700

    start styling mini palette

--- react-colors/src/PaletteList.js
+++ react-colors/src/PaletteList.js
@@ -1,17 +1,11 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import MiniPalette from "./Components/MiniPalette"
 const PaletteList = ({ palettes }) => {
   return (
     <div>
-      <MiniPalette />
       <h1>PaletteList</h1>
       {palettes.map((palette) => {
-        return (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
-        )
+        return <MiniPalette {...palette} />
       })}
     </div>
   )
