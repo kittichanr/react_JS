96be729959afad803a95d53632169a436d66a6b5
petchkubbb <>
2021-02-27 15:08:58 +0700

    add withstyles

--- react-colors/src/PaletteList.js
+++ react-colors/src/PaletteList.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
+import MiniPalette from "./Components/MiniPalette"
 const PaletteList = ({ palettes }) => {
   return (
     <div>
+      <MiniPalette />
       <h1>PaletteList</h1>
       {palettes.map((palette) => {
         return (
