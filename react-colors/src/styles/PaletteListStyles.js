2317cfee832f85f2e8f0f7b86351f0a24766f5fb
petchkubbb <>
2021-04-03 07:54:57 +0700

    fix responsive

--- react-colors/src/styles/PaletteListStyles.js
+++ react-colors/src/styles/PaletteListStyles.js
@@ -1,18 +1,31 @@
+import size from './size'
+import bg from '../images/bg.svg'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   root: {
-    backgroundColor: "blue",
-    height: "100vh",
+    height: '100vh',
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    backgroundColor: "#2f1dbd",
+    backgroundImage: `url(${bg})`,
+    overflow: 'scroll',
+  },
+  heading: {
+    fontSize: '2rem'
   },
   container: {
-    width: "50%",
+    width: "60%",
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [size.down('xl')]: {
+      width: '80%'
+    },
+    [size.down('xs')]: {
+      width: '75%'
+    }
   },
   nav: {
     display: "flex",
@@ -29,6 +42,13 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [size.down('md')]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [size.down('xs')]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.4rem",
+    }
   },
 }
